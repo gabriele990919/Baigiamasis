a48dbc5ed0671d2f4f6a79c26e8edd22112e4dfa
gabriele990919 <gabrielebanyte99@gmail.com>
2026-03-30 09:35:45 +0200

    5

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,9 +6,26 @@
     </x-slot>
 
     <div class="py-12">
-        <p>📸 Stories: {{ $storiesCount }}</p>
-        <p>❤️ Likes: {{ $likesCount }}</p>
-        <p>💸 Donated: {{ $donationsCount }} €</p>
+     <div class="flex gap-4 mb-6 justify-center">
+
+    <div class="flex-1 bg-gray-800/70 p-4 rounded-xl text-center border border-gray-700">
+        <p class="text-gray-400 text-sm">Stories</p>
+        <p class="text-2xl font-bold text-white">{{ $storiesCount }}</p>
+    </div>
+
+    <div class="flex-1 bg-gray-800/70 p-4 rounded-xl text-center border border-gray-700">
+        <p class="text-gray-400 text-sm">Likes</p>
+        <p class="text-2xl font-bold text-white">{{ $likesCount }}</p>
+    </div>
+
+    <div class="flex-1 bg-gray-800/70 p-4 rounded-xl text-center border border-gray-700">
+        <p class="text-gray-400 text-sm">Donated</p>
+        <p class="text-2xl font-bold text-green-400">{{ $donationsCount }} €</p>
+    </div>
+
+</div>
+
+</div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
